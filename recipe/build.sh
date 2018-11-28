d40bafeea95d4a9cc21235ceb334b30e13f625b5
Devon Ryan <dpryan79@gmail.com>
2018-11-28 01:06:27 +0100

    add compilers

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
-
 #!/bin/bash
+mkdir -p ~/.R
+echo "CXX=$CXX" > ~/.R/Makevars
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   mv DESCRIPTION DESCRIPTION.old
